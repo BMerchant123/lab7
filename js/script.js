36a274484a0b2c2b367835cc8e1599e2a5438638
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2022-11-06 15:10:23 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ function init(){
 //nameAlert function used to show an alert box with the contents of the text field when the button is clicked and changing the contents of the h2 text below the button to the text that the user inputted
   function nameAlert(){
 	  
-	//Outputing the alert box with the contents of the text field
-	var userInput = document.getElementById('entryinput').value;
-    alert("Bilal Merchant: "+ userInput);
+    //Outputing the alert box with the contents of the text field
+    var userInput = document.getElementById('entryinput').value;
+    alert("Bilal Merchant: " + userInput);
 	
-	//changing the value of the h2 text below the button to display the text that the user inputs
-	var alertOutput = document.getElementById('textoutput');
+    //changing the value of the h2 text below the button to display the text that the user inputs
+    var alertOutput = document.getElementById('textoutput');
     alertOutput.innerHTML = userInput;
   }
   
